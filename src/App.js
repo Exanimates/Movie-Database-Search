8ace874a8ace068c996f52c85c246537b505c6d4
Igor <exanimatesr@gmail.com>
2018-11-17 23:13:42 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <header>
+          <div id="left_header_block">
+              <img width="50" src="green_app_icon.svg"></img>
+              <h1>MovieDB Search</h1>
+          </div>      
+        </header>
+
+        <input id="search_input" placeholder="Enter search term"/>
+        <p>Kek</p>
+      </div>
+    );
+  }
+}
+
+export default App;
